25798219813acb6968696f7aee64d4cf16dc3ca7
felipe <flipeoliveira007@gmail.com>
2022-08-17 00:04:01 -0300

    att

--- src/dia_16_08_2022/contador.java
+++ src/dia_16_08_2022/contador.java
@@ -4,11 +4,23 @@ public class contador {
     public static void main(String[] args){
         System.out.println("Regressão com for: ");
         regressaoConFor(10);
+
+        System.out.println("Regressão com recursão: ");
+        regressaoComRecursao(10);
     }
 
     private static  void regressaoConFor(int valor){
         for (int i = valor; i > 0; i--) {
             System.out.println(i);
         }
+    }
+
+    private static void regressaoComRecursao(int valor){
+        System.out.println(valor);
+
+        if(valor > 0){
+            regressaoComRecursao(valor - 1);
+        }
+
     }
 }
