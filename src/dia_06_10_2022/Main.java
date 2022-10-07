8ec20875769cb9fa26a864089efc2a999674b1a1
felipe <85032728+Flipe124@users.noreply.github.com>
2022-10-07 00:11:19 -0300

    att

--- src/dia_06_10_2022/Main.java
+++ src/dia_06_10_2022/Main.java
@@ -1,6 +1,8 @@
 package dia_06_10_2022;
 
 import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.LinkedList;
 import java.util.Queue;
 
 public class Main {
@@ -8,7 +10,31 @@ public class Main {
 
 
     }
+    private static  String pesquisaEmLargura(Hashtable<String, ArrayList<String>> hashtable, String verticeInicial){
+        Queue<String> queue = new LinkedList<String>();
 
+        queue = enfilirarDados();
+
+
+
+
+        while (queue.size() > 0){
+            String pessoa = queue.poll();
+            ArrayList<String> verificados = new ArrayList<String>();
+
+            if("Harrison".equals(pessoa)){
+                return "Harrison foi encontrado!";
+            }
+
+            if (verificados.contains(pessoa)){
+                continue;
+            }
+
+            queue = enfilirarDados(queue, (ArrayList<String>) hashtable.get(pessoa));
+        }
+
+        return "Harrison não foi encontrado!";
+    }
 
 
     private static Queue<String> enfilirarDados(Queue<String> queue, ArrayList<String> lista){
